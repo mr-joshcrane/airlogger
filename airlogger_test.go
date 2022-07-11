15ad9433e2c386fb11b867b6c379e1d3921c6db7
Josh Crane <josh.crane@mechanicalrock.io>
2022-07-11 19:06:34 +0800

    Adding first test

--- airlogger_test.go
+++ airlogger_test.go
@@ -1,7 +1,19 @@
 package airlogger_test
 
-import testing
+import (
+	"testing"
+	"github.com/mr-joshcrane/airlogger"
+)
 
-func TestTest(t *testing.T) {
-	
+func TestParseArgsReturnsDagIdAndFilename(t *testing.T) {
+	t.Parallel()
+	args := []string{"airlogger", "example_dag"}
+
+	dagId, filename := airlogger.ParseArgs(args)
+	if dagId != "EXAMPLE_DAG" {
+		t.Fatalf("wanted dagId to be EXAMPLE_DAG, got  %s", dagId)
+	}
+	if filename != "example-dag.py" {
+		t.Fatalf("wanted filename to be example-dag.py, got  %s", filename)
+	}
 }
