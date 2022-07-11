abf5d87ae66a7802484039e3a48f3c1ecc98d2d7
Josh Crane <josh.crane@mechanicalrock.io>
2022-07-11 19:07:12 +0800

    Adding filenaming functionality

--- airlogger.go
+++ airlogger.go
@@ -2,8 +2,19 @@ package airlogger
 
 import (
 	"fmt"
+	"strings"
 )
 
-func main() {
-	fmt.Println("placeholder")
+func ParseArgs(args []string) (string, string) {
+	arg := args[1]
+	dagId := strings.ToUpper(arg)
+	filename := parseFilename(arg)
+	return dagId, filename
+}
+
+func parseFilename(arg string) string {
+	filename := strings.ToLower(arg)
+	filename = strings.ReplaceAll(filename, "_", "-")
+	filename = fmt.Sprintf("%s.py", filename)
+	return filename
 }
